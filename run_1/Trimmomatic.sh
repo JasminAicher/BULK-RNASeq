6794fa846bd865ab45fb5246d2cb68598c349999
Jasmin Aicher <jasmin.aicher@outlook.de>
2025-11-17 15:35:33 +0100

    update Trimmomatic

--- run_1/Trimmomatic.sh
+++ run_1/Trimmomatic.sh
@@ -10,21 +10,21 @@
 #SBATCH --tasks-per-node=1 # number of tasks per node
 #SBATCH --cpus-per-task=25 # number of allocated cores per process
 #SBATCH --mem=250G
+#SBATCH --partition=intelsr_short
 
 # Dateipräfix- und Zielordner definieren
-INPUT_DIR=jaicher_hpc@marvin.hpc.uni-bonn.de:/lustre/scratch/data/jaicher_hpc-CNTNAP2/P2025-296-RNA/
-OUTPUT_DIR=jaicher_hpc@marvin.hpc.uni-bonn.de:/lustre/scratch/data/jaicher_hpc-CNTNAP2/Trimmomatic
-ADAPTERS=jaicher_hpc@marvin.hpc.uni-bonn.de:~/adapters/TruSeq3-PE.fa
+OUTPUT_DIR=../Trimmomatic
+ADAPTERS=${HOME}/adapters/TruSeq3-PE.fa
 
 # Automatisch alle R1-Dateien finden und Sample-Namen extrahieren
-for R1 in ${INPUT_DIR}/*_R1.fq.gz; do
+for R1 in *_R1.fq.gz; do
     # Sample-Namen aus Dateinamen extrahieren
     SAMPLE=$(basename ${R1} _R1.fq.gz)
     
     echo "Verarbeite ${SAMPLE}..."
     
     # Input-Dateien
-    R2=${INPUT_DIR}/${SAMPLE}_R2.fq.gz
+    R2=${SAMPLE}_R2.fq.gz
 
     # Prüfen ob R2 existiert
     if [ ! -f ${R2} ]; then
