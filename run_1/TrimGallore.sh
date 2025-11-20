e6b0d532f847e0050ccc9ae26fd628d1567f5267
Jasmin Aicher <jasmin.aicher@outlook.de>
2025-11-20 14:57:50 +0100

    added TrimGallore

---
+++ run_1/TrimGallore.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=ag_ukb_irnb_bruestle
+#SBATCH --job-name=Gallore
+#SBATCH --output=Gallore_%j.log    # Protokoll-Datei
+#SBATCH --error=Gallore_%j.err     # Fehler-Protokoll
+#SBATCH --time=8:00:00                 # Maximale Laufzeit
+#SBATCH --partition=intelsr_short
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jaicher@uni-bonn.de  # Tipp: E-Mail-Benachrichtigung
+#SBATCH --nodes=1 # nodes requested
+#SBATCH --tasks-per-node=1 # number of tasks per node
+#SBATCH --cpus-per-task=25 # number of allocated cores per process
+#SBATCH --mem=250G
+
+if [ ! -z "${SLURM_CPUS_PER_TASK}" ] ; then
+    export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+    export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
+else
+    export OMP_NUM_THREADS=1
+    export SRUN_CPUS_PER_TASK=1
+fi
+
+trim_galore --paired --fastqc --length 20 -o trimmed DE07NGSUKBR151852_1_paired.fq.gz DE07NGSUKBR151852_2_paired.fq.gz
